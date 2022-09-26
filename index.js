d83ba68fbfcc3f03ea5f47eeca849f4c65621d75
Vidur punj <vpunj@plumslice.com>
2022-09-26 17:13:11 +0530

    Another commit

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-const express = require('express')
+const express = require('express');
 const app = express()
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser());
+
+
+const { default: mongoose } = require('mongoose');
+var moongose = require("mongoose");  // step #1 DB
+var dbURL = require("./properties").DB_URL; // db url
+moongose.connect(dbURL)
+moongose.connection.on("connected", ()=>{
+    console.log("Connected to mongo db using Mongose JS");
+})
 
 app.listen(4000)
 const path = require('path')
 
+app.get('/admin', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'admin.html'))
+    res.status(200)
+})
 app.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname, 'index.html'))
+})
+app.get('/index1', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'))
+})
+app.get('/index2', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'index2.html'))
+})
+app.get('/index3', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'index3.html'))
 })
 
 app.get('/index', (req,res)=>{
@@ -23,6 +48,26 @@ app.get('/contact',(req, res)=>{
 app.get('/post', (req,res)=>{
     res.sendFile(path.resolve(__dirname, 'post.html'))
 })
+
+app.get('/users', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'users.html'))
+})
+app.get('/sample_user', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'sample_form.html'))
+})
+app.post('/sample_user', (req,res)=>{
+    //res.sendFile(path.resolve(__dirname, 'sample_form.html'))
+    console.log(req.number)
+    console.log(req.body)
+    console.log(req.params.body)
+    console.log(req.url)
+    res.send(req.body);
+})
+
+app.get('/users.json', (req,res)=>{
+    res.json({"name": "Vidur Punj"})
+})
+
 
 
 app.use(express.static('public'))
